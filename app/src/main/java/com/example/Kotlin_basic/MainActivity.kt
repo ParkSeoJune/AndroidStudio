412588afaf6a5dba9c7fd78fd7e08817858c6a82
ParkSeoJune <tjwns4122@gmail.com>
2021-07-14 15:58:34 +0900

    20210714

--- app/src/main/java/com/example/Kotlin_basic/MainActivity.kt
+++ app/src/main/java/com/example/Kotlin_basic/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.final_practice)
+        setContentView(R.layout.final_practice_by_teacher)
     }
 }
