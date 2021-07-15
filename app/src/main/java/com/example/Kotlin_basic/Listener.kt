8cd73eaaa913d8adec1d9ad4b4b124d19ceba33e
ParkSeoJune <tjwns4122@gmail.com>
2021-07-15 20:56:08 +0900

    20210715(2)

--- app/src/main/java/com/example/Kotlin_basic/Listener.kt
+++ app/src/main/java/com/example/Kotlin_basic/Listener.kt
@@ -1,7 +1,9 @@
 package com.example.Kotlin_basic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_listener.*
 
 class Listener : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,28 @@ class Listener : AppCompatActivity() {
 //       val textView : TextView = findViewById(R.id.hello)
         // 2> xml을 import해서 가져온다
 //        hello
+
+        // 1 -> 람다 방식
+        hello.setOnClickListener {
+            Log.d("click", "Click!")
+        }
+
+//        // 2 -> 익명함수 방식
+//        hello.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Log.d("click", "Click!")
+//            }
+//        })
+//
+//        // 3 -> 이름이 필요한 경우(click)
+//        val click = object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                Log.d("click", "Click!")
+//            }
+//        }
+//
+//        hello.setOnClickListener(click)
+
 
     }
 
