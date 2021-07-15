8212adf41570b0a45476f6d1f2dd8e4deaa86932
ParkSeoJune <tjwns4122@gmail.com>
2021-07-15 20:26:10 +0900

    20210715

---
+++ app/src/main/java/com/example/Kotlin_basic/Listener.kt
@@ -0,0 +1,20 @@
+package com.example.Kotlin_basic
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Listener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listener)
+
+        // 뷰를 activity로 가져오는 방법법
+//       val textView : TextView = findViewById(R.id.hello)
+        // 2> xml을 import해서 가져온다
+//        hello
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/Kotlin_basic/MainActivity.kt
+++ app/src/main/java/com/example/Kotlin_basic/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.Kotlin_basic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.final_practice_by_teacher)
+
+        Log.d("life_cycle","onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle","onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle","onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle","onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle","onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle","onDestroy")
+
     }
 }
