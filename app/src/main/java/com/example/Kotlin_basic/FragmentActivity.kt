39c8a96a8626d8381051f84d9e3610a884564832
PSJ <  >
2021-09-15 10:48:35 +0900

    Fragmen실습1(20210914)

---
+++ app/src/main/java/com/example/Kotlin_basic/FragmentActivity.kt
@@ -0,0 +1,43 @@
+package com.example.Kotlin_basic
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class FragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        Log.d("life_cycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle","onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle","onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle","onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle","onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle","onDestroy")
+
+    }
+}
